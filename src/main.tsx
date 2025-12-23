330f25c1d64ea04e194447e16c6b6c554a40cab6
Sachin Titus <sachintitusin@gmail.com>
2025-12-23 15:32:35 +0530

    removed the model ensuringcode from main

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,6 @@ import './index.css'
 import App from './App.tsx'
 
 
-// Ensure modal-root exists before rendering the app
-const ensureModalRoot = () => {
-  if (!document.getElementById('modal-root')) {
-    const modalDiv = document.createElement('div');
-    modalDiv.id = 'modal-root';
-    document.body.appendChild(modalDiv);
-  }
-}
-
-ensureModalRoot();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
